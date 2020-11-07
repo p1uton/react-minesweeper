c4453b0a8e8c84658f840ebda9f0e22c6953b731
p1uton <p1uton@users.noreply.github.com>
2020-11-07 12:00:57 +0800

    Updates

---
+++ precache-manifest.ba8bb9068bb146ad298e12b85429214c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f3b509afc122c603890e0757e205159",
+    "url": "/react-minesweeper/index.html"
+  },
+  {
+    "revision": "3f76b225a9949d99a4a1",
+    "url": "/react-minesweeper/static/css/main.1f92bcc1.chunk.css"
+  },
+  {
+    "revision": "b7db8f3c67d0da60cadf",
+    "url": "/react-minesweeper/static/js/2.1a7576f5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-minesweeper/static/js/2.1a7576f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f76b225a9949d99a4a1",
+    "url": "/react-minesweeper/static/js/main.5610f899.chunk.js"
+  },
+  {
+    "revision": "3641de6aa71eef7e8bd1",
+    "url": "/react-minesweeper/static/js/runtime-main.4a604056.js"
+  }
+]);
